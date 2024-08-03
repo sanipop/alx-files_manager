02b6eda328eb8545f8a4b9990914714a21087254
sanipop <67932273+sanipop@users.noreply.github.com>
2024-08-03 11:13:25 +0100

    Create redis.js

---
+++ utils/redis.js
@@ -0,0 +1,44 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+// client merhod
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+  }
+
+  // methid to check connect status
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  // method tobget value
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
+
+  // key val pair for server
+  async set(key, value, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    await this.client.expire(key, time);
+  }
+
+  // merhod to del key val pair
+  async del(key) {
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
